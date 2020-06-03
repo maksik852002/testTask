f3ab0412120408617d29a58c396ebb24de66d26f
Maxim Konovalov <maksik852002@gmail.com>
2020-06-03 11:15:19 +0600

    add new column width

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -10,12 +10,12 @@ export const defaultAvatar =
   "https://www.unipulse.tokyo/en/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png";
   
 export const columns = [
-  { id: "title", label: "Title", width: "calc((100% - 0px) / 3)" },
-  { id: "category", label: "Category", width: "calc((100% - 0px) / 3)" },
+  { id: "title", label: "Title", width: "calc((100% - 0px) / 2)" },
+  { id: "category", label: "Category", width: "calc((100% - 0px) / 4)" },
   {
     id: "createdBy",
     label: "Created by",
-    width: "calc((100% - 0px) / 3)",
+    width: "calc((100% - 0px) / 4)",
   },
   {
     id: "actions",
